f5a0e533718800b59f37972806f529679b069f62
Cj Alexander <clauvinsalexander@gmail.com>
2024-05-11 21:54:05 -0400

    Completed pop method

--- linkedlist.js
+++ linkedlist.js
@@ -1,78 +1,135 @@
 class LinkedList {
-    constructor() {
-        this.headNode = null;
-        this.tailNode = null;
-        this.length = 0;
+  constructor() {
+    this.headNode = null;
+    this.tailNode = null;
+    this.length = 0;
+  }
+
+  append(data) {
+    if (this.length === 0) {
+      this.headNode = new Node(data);
+    } else {
+      if (this.tailNode !== null) {
+        const prevNode = this.tailNode;
+        const newNode = new Node(data);
+        prevNode.setNextNode(newNode);
+        this.tailNode = newNode;
+      } else {
+        this.tailNode = new Node(data);
+        this.headNode.setNextNode(this.tailNode);
+      }
     }
 
-    append(data) {
-        if (this.length === 0) {
-            this.headNode = new Node(data);
-        } else {
-            if (this.tailNode !== null) {
-                const prevNode = this.tailNode;
-                const newNode = new Node(data);
-                prevNode.setNextNode(newNode);
-                this.tailNode = newNode;
-            } else {
-                this.tailNode = new Node(data);
-                this.headNode.setNextNode(this.tailNode)
-            }
-        }
+    this.length++;
+  }
 
-        this.length++
+  prepend(data) {
+    if (this.length === 0) {
+      this.headNode = new Node(data);
+    } else {
+      const newNode = new Node(data);
+      newNode.setNextNode(this.headNode);
+      this.headNode = newNode;
     }
 
-    prepend(data) {
-        if (this.length === 0) {
-            this.headNode = new Node(data);
-        } else {
-            const newNode = new Node(data);
-            newNode.setNextNode(this.headNode)
-            this.headNode = newNode
-        }
+    this.length++;
+  }
 
-        this.length++
+  size() {
+    console.log(this.length);
+  }
+
+  head() {
+    console.log(this.headNode);
+  }
+
+  tail() {
+    console.log(this.tailNode);
+  }
+
+  at(index) {
+    let currentNode = this.headNode;
+    let found = false;
+    for (let i = 0; i < this.length; i++) {
+      if (i == index) {
+        found = true;
+        console.log(currentNode);
+      }
+
+      currentNode = currentNode.getNextNode();
     }
 
-    toString() {
-        let resultString = ''
-        let currentNode = this.headNode;
-        for (let i = 0; i < this.length; i++) {
-            resultString += `( ${currentNode.getNodeData()} ) -> `
-            currentNode = currentNode.getNextNode();
-            
-            if (currentNode === null) {
-                resultString += 'null'
-            }
-        }
+    if (!found) console.log("Node not found.");
+  }
 
-        console.log(resultString)
+  pop() {
+    if (this.length === 0) {
+      console.log("No nodes to remove.");
+      return;
     }
+
+    if (this.length === 1) {
+      this.headNode = null;
+      this.tailNode = null;
+      this.length = 0;
+      return;
+    }
+
+    let currentNode = this.headNode;
+    let prevNode = null;
+    // Traverse until the second last node
+    while (currentNode.getNextNode() !== null) {
+      prevNode = currentNode;
+      currentNode = currentNode.getNextNode();
+    }
+
+    if (prevNode !== null) {
+      prevNode.setNextNode(null);
+      this.tailNode = prevNode;
+    }
+
+    this.length--;
+  }
+
+  toString() {
+    let resultString = "";
+    let currentNode = this.headNode;
+    for (let i = 0; i < this.length; i++) {
+      resultString += `( ${currentNode.getNodeData()} ) -> `;
+      currentNode = currentNode.getNextNode();
+
+      if (currentNode === null) {
+        resultString += "null";
+      }
+    }
+
+    console.log(resultString);
+  }
 }
-
 
 class Node {
-    constructor(data) {
-        this.data = data;
-        this.nextNode = null;
-    }
+  constructor(data) {
+    this.data = data;
+    this.nextNode = null;
+  }
 
-    setNextNode(node) {
-        this.nextNode = node
-    }
+  setNextNode(node) {
+    this.nextNode = node;
+  }
 
-    getNextNode() {
-        return this.nextNode;
-    }
+  getNextNode() {
+    return this.nextNode;
+  }
 
-    getNodeData() {
-        return this.data
-    }
+  getNodeData() {
+    return this.data;
+  }
 }
 
-let test = new LinkedList()
-test.append(4)
-test.append(5)
-test.prepend(6)
-test.toString()
+let test = new LinkedList();
+test.append(4);
+test.append(5);
+test.append(6);
+test.toString();
+test.pop()
+test.toString();
